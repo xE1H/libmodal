b0ea2fe8b75e753ead1949dc222cf83e7d394a0e
Eric Zhang <ekzhang1@gmail.com>
2025-04-15 17:32:15 +0000

    Instantiate messy typescript protobuf stuff

--- non-native/README.md
+++ non-native/README.md
@@ -1,10 +1,28 @@
-Run scripts with:
+# non-native
+
+Setup after cloning the repo:
 
 ```bash
 npm install
+
+./node_modules/.bin/grpc_tools_node_protoc \
+  --plugin=protoc-gen-ts_proto=./node_modules/.bin/protoc-gen-ts_proto \
+  --ts_proto_out=./proto \
+  --ts_proto_opt=outputServices=nice-grpc,outputServices=generic-definitions,useExactTypes=false \
+  --proto_path=../modal-client \
+  ../modal-client/modal_proto/*.proto
+```
+
+Then run a script with:
+
+```bash
 node --import tsx path/to/script.ts
 ```
 
 This isn't meant to be a clean package or library setup, it's a minimal experiment for now.
 
 Not going to mess with `tsup` or other bundlers.
+
+## gRPC support
+
+We're using `nice-grpc` because the `@grpc/grpc-js` library kind of sucks, it doesn't even use promises? What's going on with this part of the ecosystem.
--- non-native/src/main.ts
+++ non-native/src/main.ts
@@ -1,1 +1,37 @@
-console.log("Hello!");
+import { ModalClientDefinition } from "../proto/modal_proto/api.ts";
+import {
+  CallOptions,
+  ClientMiddlewareCall,
+  createChannel,
+  createClientFactory,
+  Metadata,
+} from "nice-grpc";
+
+const tokenId = process.env["MODAL_TOKEN_ID"];
+const tokenSecret = process.env["MODAL_TOKEN_SECRET"];
+
+if (!tokenId || !tokenSecret) {
+  throw new Error(
+    "Please set the MODAL_TOKEN_ID and MODAL_TOKEN_SECRET environment variables."
+  );
+}
+
+const channel = createChannel("https://api.modal.com:443");
+const client = createClientFactory()
+  .use(async function* middleware<Request, Response>(
+    call: ClientMiddlewareCall<Request, Response>,
+    options: CallOptions
+  ) {
+    options.metadata ??= new Metadata();
+    options.metadata.set("x-modal-client-type", "1");
+    options.metadata.set("x-modal-client-version", "424242");
+    options.metadata.set("x-modal-token-id", tokenId);
+    options.metadata.set("x-modal-token-secret", tokenSecret);
+    return yield* call.next(call.request, options);
+  })
+  .create(ModalClientDefinition, channel);
+
+const resp = await client.clientHello({});
+console.log(resp);
+
+console.log("Connected!");
