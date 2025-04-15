0be0eb4805dc0f842c7de1a966e05ebce3f4a93c
Eric Zhang <ekzhang1@gmail.com>
2025-04-15 17:47:26 +0000

    Create a sandbox

---
+++ non-native/src/client.ts
@@ -0,0 +1,32 @@
+import { ModalClientDefinition } from "../proto/modal_proto/api.ts";
+import {
+  CallOptions,
+  ClientMiddlewareCall,
+  createChannel,
+  createClientFactory,
+  Metadata,
+} from "nice-grpc";
+
+const tokenId = process.env["MODAL_TOKEN_ID"];
+const tokenSecret = process.env["MODAL_TOKEN_SECRET"];
+
+if (!tokenId || !tokenSecret) {
+  throw new Error(
+    "Please set the MODAL_TOKEN_ID and MODAL_TOKEN_SECRET environment variables."
+  );
+}
+
+const channel = createChannel("https://api.modal.com:443");
+export const client = createClientFactory()
+  .use(async function* middleware<Request, Response>(
+    call: ClientMiddlewareCall<Request, Response>,
+    options: CallOptions
+  ) {
+    options.metadata ??= new Metadata();
+    options.metadata.set("x-modal-client-type", "1");
+    options.metadata.set("x-modal-client-version", "424242");
+    options.metadata.set("x-modal-token-id", tokenId);
+    options.metadata.set("x-modal-token-secret", tokenSecret);
+    return yield* call.next(call.request, options);
+  })
+  .create(ModalClientDefinition, channel);
--- non-native/src/main.ts
+++ non-native/src/main.ts
@@ -1,37 +1,24 @@
-import { ModalClientDefinition } from "../proto/modal_proto/api.ts";
-import {
-  CallOptions,
-  ClientMiddlewareCall,
-  createChannel,
-  createClientFactory,
-  Metadata,
-} from "nice-grpc";
-
-const tokenId = process.env["MODAL_TOKEN_ID"];
-const tokenSecret = process.env["MODAL_TOKEN_SECRET"];
-
-if (!tokenId || !tokenSecret) {
-  throw new Error(
-    "Please set the MODAL_TOKEN_ID and MODAL_TOKEN_SECRET environment variables."
-  );
-}
-
-const channel = createChannel("https://api.modal.com:443");
-const client = createClientFactory()
-  .use(async function* middleware<Request, Response>(
-    call: ClientMiddlewareCall<Request, Response>,
-    options: CallOptions
-  ) {
-    options.metadata ??= new Metadata();
-    options.metadata.set("x-modal-client-type", "1");
-    options.metadata.set("x-modal-client-version", "424242");
-    options.metadata.set("x-modal-token-id", tokenId);
-    options.metadata.set("x-modal-token-secret", tokenSecret);
-    return yield* call.next(call.request, options);
-  })
-  .create(ModalClientDefinition, channel);
+import { NetworkAccess_NetworkAccessType } from "../proto/modal_proto/api.ts";
+import { client } from "./client.ts";
 
 const resp = await client.clientHello({});
 console.log(resp);
 
 console.log("Connected!");
+
+const sandboxCreateResp = await client.sandboxCreate({
+  appId: "ap-tBJPwfXSZCUeRdF1mzH5fL",
+  definition: {
+    entrypointArgs: ["echo", "hello"],
+    imageId: "im-0MT7lcT3Kzh7DxZgVHgSRY",
+    timeoutSecs: 600,
+    networkAccess: {
+      networkAccessType: NetworkAccess_NetworkAccessType.OPEN,
+    },
+    resources: {
+      memoryMb: 1024,
+      milliCpu: 1000,
+    },
+  },
+});
+console.log(sandboxCreateResp);
